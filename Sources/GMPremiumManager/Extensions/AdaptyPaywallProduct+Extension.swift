b2cd1499b020d196e3556d0ea4fa7574e69641dd
Danilo <dh@goingmerry.xyz>
2025-06-03 10:13:15 -0300

    Update deprecated locale on weeklyformattedprice extension

--- Sources/GMPremiumManager/Extensions/AdaptyPaywallProduct+Extension.swift
+++ Sources/GMPremiumManager/Extensions/AdaptyPaywallProduct+Extension.swift
@@ -24,17 +24,18 @@ extension AdaptyPaywallProduct {
 public extension AdaptyPaywallProduct {
     func weeklyFormattedPrice() -> String? {
         guard let subscriptionPeriodUnit = self.subscriptionPeriod?.unit else { return nil }
+        let locale = self.sk2Product?.priceFormatStyle.locale
 
         switch subscriptionPeriodUnit {
         case .month:
             let price = (self.price as NSNumber).doubleValue / 4
-            return (price as NSNumber).getPrice(for: self.sk1Product?.priceLocale)
+            return (price as NSNumber).getPrice(for: locale)
         case .year:
-            let price = (self.price as NSNumber).doubleValue / 52
-            return (price as NSNumber).getPrice(for: self.sk1Product?.priceLocale)
+            let numberOfWeeksInAYear = Calendar.current.maximumRange(of: .weekOfYear)?.upperBound ?? 52
+            let price = (self.price as NSNumber).doubleValue / Double(numberOfWeeksInAYear)
+            return (price as NSNumber).getPrice(for: locale)
         default: break
         }
-
-        return (price as NSNumber).getPrice(for: self.sk1Product?.priceLocale)
+        return (price as NSNumber).getPrice(for: locale)
     }
 }
